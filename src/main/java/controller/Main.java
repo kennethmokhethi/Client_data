591262281116153fb266d775daa151168332833c
Kenneth Mokhethi <Kenneth.mokhethi@investec.co.za>
2020-04-04 20:56:14 +0200

    Added data to the database

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,7 @@
 package controller;
 
 import model.Client;
-
+import dao.Client_DAO;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,11 +12,13 @@ import java.io.PrintWriter;
 
 @WebServlet("/Main")
 public class Main extends HttpServlet {
-  private Client client;
+  private Client_DAO client_dao;
 
      @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
      {
+         client_dao =new Client_DAO();
+
 //         doPost(request,response);
          response.setContentType("text/html");
 
@@ -26,13 +28,33 @@ public class Main extends HttpServlet {
          String strAcc_num = request.getParameter("cAcc_num");
 
 
-         client = new Client(Name,LastName,Address,Integer.parseInt(strAcc_num));
+         Client  obj_client = new Client(Name,LastName,Address,Integer.parseInt(strAcc_num));
 
+         try
+         {
+             //create the table
+             client_dao.create_client_table();
+
+             //add client data
+           boolean validate_entry= client_dao.add_Client(obj_client);
+
+           if(validate_entry)
+           {
+               System.out.println("Data saved");
+           }else{
+               System.out.println("Something gone wrong!!");
+           }
+
+
+         }catch(Exception ex)
+         {
+             ex.printStackTrace();
+         }
 
 
 
          PrintWriter output = response.getWriter();
-         output.println("<h2>" + client.getName()+"</h2>");
+         output.println("<h2>" + obj_client.getName()+"</h2>");
 
      }
 
--- src/main/java/dao/Client_DAO.java
+++ src/main/java/dao/Client_DAO.java
@@ -2,7 +2,6 @@ package dao;
 
 import connection.cConnection;
 import model.Client;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -18,30 +17,30 @@ public class Client_DAO {
     private Statement retrieve_data_from_db;
     private ResultSet output_from_db;
 
-    private void create_client_table()
+    public void create_client_table()
     {
         try
         {
             conn = cConnection.getConnection();
             create_stmt = conn.createStatement();
-            String  create_client_table_sql ="CREATE TABLE IF NOT EXIST Client(Name varchar(100),LastName varchar(100),Address varchar(100),Acc_num int primary key)";
+            String  create_client_table_sql ="CREATE TABLE IF NOT EXISTS CLIENT(C_Name varchar(100),LastName varchar(100),Address varchar(100),Acc_num int primary key)";
             create_stmt.executeUpdate(create_client_table_sql);
 
         }catch(Exception ex)
         {
-            ex.printStackTrace();
+            System.out.println("Error on create table "+ex);
         }finally {
             close(conn,create_stmt,null);
         }
     }
 
 
-    private boolean add_Client(Client obj_client)
+    public boolean add_Client(Client obj_client)
     {
         try
         {
             conn = cConnection.getConnection();
-            String add_client_data ="INSERT INTO Client(Name,LastName,Address,Acc_num) VALUES(?,?,?,?)";
+            String add_client_data ="INSERT INTO Client(C_Name,LastName,Address,Acc_num) VALUES(?,?,?,?)";
             PreparedStatement client_ps = conn.prepareStatement(add_client_data);
             client_ps.setString(1,obj_client.getName());
             client_ps.setString(2,obj_client.getLastname());
@@ -50,7 +49,7 @@ public class Client_DAO {
             client_ps.executeUpdate();
         }catch(Exception ex)
         {
-            ex.printStackTrace();
+            System.out.println("Error in insert "+ex);
         }finally {
             //close connection
             close(conn,null,null);
@@ -59,7 +58,7 @@ public class Client_DAO {
         return true;
     }
 
-    private ArrayList<Client> getClients()
+    public ArrayList<Client> getClients()
     {
         try
         {
