2e698573f612740ab55d5e565e809f9034047d63
Kenneth Mokhethi <Kenneth.mokhethi@investec.co.za>
2020-04-04 16:37:20 +0200

    Added the client class

---
+++ src/main/java/controller/Main.java
@@ -0,0 +1,13 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+
+
+    
+
+
+}
---
+++ src/main/java/model/Client.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Client{
+    private String name;
+    private String surname;
+    private String address;
+    private int acc_num;
+
+
+    public Client(String name,String surname,String address,int acc_num)
+    {
+        setName(name);
+        setSurname(surname);
+        setAddress(address);
+        setAcc_num(acc_num);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAcc_num() {
+        return acc_num;
+    }
+
+    public void setAcc_num(int acc_num) {
+        this.acc_num = acc_num;
+    }
+}
